0a188e938d4d955d90345bbf5c4b3110a41d1661
Mohammad Nihaal <143014604+mohammadnihaal@users.noreply.github.com>
2025-04-16 00:31:27 +0530

    Add files via upload

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {socialImgs} from "../constants/index.js";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="flex flex-col justify-center md:items-start items-center">
+                    <a href="/">Visit My Portfolio</a>
+                </div>
+                <div className="socials">
+                    {socialImgs.map((img) => (
+                        <a className="icon" target="_blank" href={img.url} key={img.url}>
+                            <img src={img.imgPath} />
+                        </a>
+                    ))}
+                </div>
+                <div className="flex flex-col justify-center">
+                    <p className="text-center md:text-end">
+                        © {new Date().getFullYear()} | Nihaal | All Rights Reserved.
+                    </p>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
