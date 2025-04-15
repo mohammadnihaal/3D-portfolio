c18019a4e4eab419ea18e474dedf2d87d148d137
Mohammad Nihaal <143014604+mohammadnihaal@users.noreply.github.com>
2025-04-16 00:32:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Hero from "./sections/Hero.jsx";
+import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
+import LogoSection from "./sections/LogoSection.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
+import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
+
+const App = () => {
+    return (
+        <>
+            <NavBar />
+            <Hero />
+            <ShowcaseSection />
+            {/* <LogoSection /> */}
+            <FeatureCards />
+            <ExperienceSection />
+            <TechStack />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+export default App
